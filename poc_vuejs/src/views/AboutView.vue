a2c8ff4278776751e097891a1c97c5ae6ded4bc5
Kibishi47 <78955048+Kibishi47@users.noreply.github.com>
2024-03-10 13:54:00 +0100

    add(route): new route named test to try vuerouter

--- poc_vuejs/src/views/AboutView.vue
+++ poc_vuejs/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link :to="{name:'test', query: { username: 'pseudoTest', pageFrom: 'about'}}">Go to Test with Data</router-link>
   </div>
 </template>
---
+++ poc_vuejs/src/views/TestView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <button>TEST</button>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    const username = this.$route.query.username;
+    if (username) {
+      console.log(username);
+    }
+  }
+}
+</script>
